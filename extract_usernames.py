ec1c945b240dcb53cc22b6cedf4c988523160261
Tracey L <traceyle@outlook.com>
2017-06-09 21:59:48 +1000

    Add files via upload

---
+++ extract_usernames.py
@@ -0,0 +1,16 @@
+import re
+username_list = []
+
+# Text is stored in text_file.txt
+with open('forumtext.txt') as text_file:
+    for line in text_file:
+        # Find all words in the line
+        # that match the format of starting with @ and have 3+ characters (dodgy matching)
+        matched_usernames = re.findall(r'@[\w|\.|-]{3,}', line)
+        for match in matched_usernames:
+             # Store name without @
+            username_list.append(match[1:])
+
+# Print out list of names to copy and paste
+for username in username_list:
+    print(username)
